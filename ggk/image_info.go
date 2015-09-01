1cdfb99a8fd24144b571a9f4c46a10a2f112fdaf
sj20082663 <shijian0912@163.com>
2015-09-01 23:10:02 +0800

    add image_info_test modified image_info

--- ggk/image_info.go
+++ ggk/image_info.go
@@ -2,36 +2,48 @@ package ggk
 
 // Alpha types
 // Describe how to interpret the alpha compoent of a pixel.
+type AlphaType int
+
 const (
-	AlphaUnknown = iota
+	AlphaTypeUnknown AlphaType = iota
 
 	// All pixels are stored as opaque. This differs slightly from kIgnore in
 	// that kOpaque has correct "Opaque" values stored in the pixels, while
 	// kIgnore may not, but in both cases the caller should treat the pixels
 	// as opaque.
-	AlphaOpaque
+	AlphaTypeOpaque
 
 	// All pixels have their alpha premultiplied in their color components.
 	// This is the natural format for the rendering target pixels.
-	AlphaPremul
+	AlphaTypePremul
 
 	// All pixels have their color components stroed without any regard to the
 	// alpha. e.g. this is the default configuration for PNG images.
 	//
 	// This alpha-type is ONLY supported for input images. Rendering cannot
 	// generate this on output.
-	AlphaUnpremul
+	AlphaTypeUnpremul
 
-	AlphaLast = AlphaUnpremul
+	AlphaTypeLastEnum = AlphaTypeUnpremul
 )
+
+func AlphaTypeIsOpaque(alphaType AlphaType) bool {
+	return alphaType == AlphaTypeOpaque
+}
+
+func AlphaTypeIsValid(value AlphaType) bool {
+	return value >= 0 && value <= AlphaTypeLastEnum
+}
 
 // Color types
 // Describes how to interpret the components of a pixel.
 // ColorTypeN32 is an alias for whichever 32bit ARGB format is the "native"
 // form for blitters. Use this if you don't hava a swizzle preference
 // for 32bit pixels.
+type ColorType int
+
 const (
-	ColorTypeUnknown = iota
+	ColorTypeUnknown ColorType = iota
 	ColorTypeAlpha8
 	ColorTypeRGB565
 	ColorTypeARGB4444
@@ -39,12 +51,13 @@ const (
 	ColorTypeBGRA8888
 	ColorTypeIndex8
 	ColorTypeGray8
-	ColorTypeLast = ColorTypeGray8
+
+	ColorTypeLastEnum = ColorTypeGray8
 )
 
-func ColorTypeBytesPerPixel(colorType uint) uint {
+func ColorTypeBytesPerPixel(colorType ColorType) int {
 	// TODO: const?
-	var bytesPerPixel = []uint{
+	var bytesPerPixel = []int{
 		0, // Unknown
 		1, // Alpha8
 		2, // RGB565
@@ -55,30 +68,22 @@ func ColorTypeBytesPerPixel(colorType uint) uint {
 		1, // Gray8
 	}
 
-	if colorType >= uint(len(bytesPerPixel)) {
+	if colorType >= ColorType(len(bytesPerPixel)) {
 		return 0
 	}
 
 	return bytesPerPixel[colorType]
 }
 
-func ColorTypeMinRowBytes(colorType uint, width uint) uint {
+func ColorTypeMinRowBytes(colorType ColorType, width int) int {
 	return width * ColorTypeBytesPerPixel(colorType)
 }
 
-func ColorTypeIsVaild(value uint) bool {
-	return value <= ColorTypeLast
+func ColorTypeIsVaild(value ColorType) bool {
+	return value <= ColorTypeLastEnum
 }
 
-func AlphaTypeIsOpaque(alphaType uint) bool {
-	return alphaType == AlphaOpaque
-}
-
-func AlphaTypeIsValid(value uint) bool {
-	return value <= AlphaLast
-}
-
-func ColorTypeComputeOffset(colorType uint, x, y int, rowBytes uint) uint {
+func ColorTypeComputeOffset(colorType ColorType, x, y int, rowBytes int) int {
 	var shift uint = 0
 
 	switch ColorTypeBytesPerPixel(colorType) {
@@ -92,20 +97,22 @@ func ColorTypeComputeOffset(colorType uint, x, y int, rowBytes uint) uint {
 		return 0
 	}
 
-	return uint(y)*rowBytes + uint(x)<<shift
+	return y*rowBytes + x<<shift
 }
 
 // Return true if alphaType is supported by colorType. If there is a canonical
 // alphaType for this colorType, return it in canonical.
-func ColorTypeValidateAlphaType(colorType uint, alphaType uint) (canonical uint) {
+func ColorTypeValidateAlphaType(colorType ColorType, alphaType AlphaType) AlphaType {
 	return 0
 }
 
 // YUV color space
 // Describes the color space a YUV pixel
+type YUVColorSpace int
+
 const (
 	// Standard JPEG color space.
-	YuvColorSpaceJPEG = iota
+	YUVColorSpaceJPEG YUVColorSpace = iota
 	// SDTV standard Rec. 601 color space. Uses "studio swing" [16, 245] color
 	// range. See http://en.wikipedia.org/wiki/Rec._601 for details.
 	YUVColorSpaceRec601
@@ -113,14 +120,17 @@ const (
 	// range. See http://en.wikipedia.org/wiki/Rec._709 for details.
 	YUVColorSpaceRec709
 
-	YUVColorSpaceLast = YUVColorSpaceRec709
+	YUVColorSpaceLastEnum = YUVColorSpaceRec709
 )
 
 // Color profile type
+type ColorProfileType int
+
 const (
-	ColorProfileTypeLinear = iota
+	ColorProfileTypeLinear ColorProfileType = iota
 	ColorProfileTypeSRGB
-	ColorProfileTypeLast = ColorProfileTypeSRGB
+
+	ColorProfileTypeLastEnum = ColorProfileTypeSRGB
 )
 
 // Describe an image's dimensions and pixel type.
@@ -129,11 +139,36 @@ type ImageInfo struct {
 	Width  int
 	Height int
 
-	ColorType   uint
-	AlphaType   uint
-	ProfileType uint
+	ColorType   ColorType
+	AlphaType   AlphaType
+	ProfileType ColorProfileType
 }
 
-func (imageInfo *ImageInfo) BytesPerPixel() uint {
+func (imageInfo *ImageInfo) BytesPerPixel() int {
 	return ColorTypeBytesPerPixel(imageInfo.ColorType)
+}
+
+func New(width, height int, colorType ColorType, alphaType AlphaType,
+	profileType ColorProfileType) *ImageInfo {
+	var imageInfo = &ImageInfo{
+		Width:       width,
+		Height:      height,
+		ColorType:   colorType,
+		AlphaType:   alphaType,
+		ProfileType: profileType,
+	}
+
+	return imageInfo
+}
+
+func NewN32() {
+
+}
+
+func NewN32Premul() {
+
+}
+
+func NewA8() {
+
 }
---
+++ ggk/image_info_test.go
@@ -0,0 +1,29 @@
+package ggk_test
+
+import (
+	"gwk/ggk"
+	"testing"
+)
+
+var ctTests = []struct {
+	ct       ggk.ColorType
+	numBytes int
+}{
+	{ggk.ColorTypeUnknown, 0},
+	{ggk.ColorTypeAlpha8, 1},
+}
+
+func TestColorType(t *testing.T) {
+	var (
+		ct       ggk.ColorType
+		numBytes int
+	)
+
+	for _, tt := range ctTests {
+		ct = tt.ct
+		numBytes = ggk.ColorTypeBytesPerPixel(ct)
+		if numBytes != tt.numBytes {
+			t.Errorf("ColorTypeBytesPerPixel(%d) -> %d expect %d", ct, numBytes, tt.numBytes)
+		}
+	}
+}
